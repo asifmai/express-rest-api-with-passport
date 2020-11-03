d2b3bdb3578f40a11660531e6950498eb3aabcf9
Iqbal <asifmai@hotmail.com>
2020-11-03 23:11:38 +0500

    update

--- config/db.js
+++ config/db.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
+module.exports = () => new Promise(async (resolve, reject) => {
   try {
+    // Connect to MongoDB
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
+      useFindAndModify: false,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
     });
-
+    // mongoose.set('debug', true);
     console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
 
-module.exports = connectDB;
+    resolve(true);
+  } catch (error) {
+    console.log('MongoDB Connection Error: ', error);
+    reject(error);
+  }
+});
