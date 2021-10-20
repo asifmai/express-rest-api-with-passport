4d15bf82bed92451d7fcd0bbe0e81be6aecd30ee
Iqbal <asifmai@hotmail.com>
2021-10-20 21:26:06 +0500

    'update'

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,14 +1,18 @@
-const jwt = require('jsonwebtoken');
-
-function generateTokenResponse(user) {
-  return {
-    token: 'Bearer ' + jwt.sign(userInfo, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRY }),
-    user,
-  };
-}
+const generateToken = require('../helpers/generatetoken');
+const passport = require('passport');
 
 module.exports.login_post = async function (req, res, next) {
-  return res.status(200).json({ status: 200, data: generateTokenResponse(req.user) });
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ status: 400, data: 'Unauthorized' });
+    }
+    if (!user) {
+      console.log('User not found');
+      return res.status(400).json({ status: 400, data: 'Unauthorized' });
+    }
+    return res.status(200).json({ status: 200, data: generateToken(user) });
+  })(req, res, next);
 };
 
 module.exports.me_get = (req, res, next) => {
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const userController = require('../controllers/userscontroller');
 const authController = require('../controllers/authcontroller');
-
-// Authenticate Using Passport
-const login = passport.authenticate('local', { session: false });
-const apiAuth = passport.authenticate('jwt', { session: false });
-
-router.post('/', (req, res) => res.status(200).json({ status: 200, data: 'API Running' }));
+const authMw = require('../config/apiauth');
 
 // User Routes
+router.post('/', (req, res) => res.status(200).json({ status: 200, data: 'API Running' }));
 router.post('/users/register', userController.register_post);
-router.post('/users/login', login, authController.login_post);
-router.get('/users/me', apiAuth, authController.me_get);
+router.post('/users/login', authController.login_post);
+router.get('/users/me', authMw.ensureAuthenticated, authController.me_get);
 
 module.exports = router;
